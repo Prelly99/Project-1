d083e7d133cbe3a555d1303ee50a6693e9e8b38d
James Prell <james.prell@uon.edu.au>
2019-04-03 00:48:43 +0000

    Converted single lowercase characters into uppercase

--- Start.c
+++ Start.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-main int() {
+int main() {
+    char a; //initialising the imput from the user
+    
+    printf("text to encrypt: ");
+    scanf("%s", &a);
+    
+    if(a > 97){
+    a = a-32; //converts all lowercase imputs into upercase imputs
+    }
 
+    printf("read: %c\n", a);
+
+   
+    
     return 0;
 }
