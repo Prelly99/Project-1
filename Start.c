0baaa861a7320eb8ee6053b72a892b755dd7b9d2
James Prell <james.prell@uon.edu.au>
2019-04-23 03:28:54 +0000

    rotation cipher with variable key

--- Start.c
+++ Start.c
@@ -2,27 +2,33 @@
 
 int main() {
     
+    
+    
+        int key;
+        printf("enter key ", key);
+        scanf("%d", &key); 
         
         FILE *input;
     
-        input = fopen("input.txt", "r"); //opening the file input
+        input = fopen("input.txt", "r"); //opening the file 'input.txt'
     
         while(feof(input) == 0){
-        char c; //initialising the imput from the user
-        fscanf(input, "%c", &c);
+            char c; //initialising the imput from the user
+            fscanf(input, "%c", &c);
        
         
        
       
             if(c > 96){
-                c = c-32; //converts all lowercase imputs into upercase imputs
+                c = c - 32; //converts all lowercase imputs into upercase imputs
             }
-            if(c > 65 && c < 91){
-                c = c - 1; //roatates the ascii code of each character down by 1
+            if(c > 64 && c < 91){
+                c = c - key; //roatates the ascii code of each character down by 1
+                if(c < 64){
+                    c = c + 26;
+                }
             }
-            else if(c == 65){
-                c = c + 25;
-            }
+            
             printf("%c", c);
         }
         
